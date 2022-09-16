483123567e2f0f7dbd73ca1c2fcc49c8e61a8294
norajas <113666841+norajas@users.noreply.github.com>
2022-09-16 10:17:15 +0300

    Create mano_lib.hpp

---
+++ mano_lib.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::sort;
+using std::left;
+using std::setw;
