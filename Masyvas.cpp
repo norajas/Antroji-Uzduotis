8c39d682061d126b1d233e57719e2bfc94e2dcd3
norajas <113666841+norajas@users.noreply.github.com>
2022-09-16 10:16:26 +0300

    Create Masyvas.cpp

---
+++ Masyvas.cpp
@@ -0,0 +1,59 @@
+#include "mano_lib.hpp"
+
+struct duom{
+  string vardas;
+  string pavarde;
+  double vid;
+  double med;
+  int egz;
+};
+
+duom ivedimas(int &n);
+void isvedimas(duom temp, int n);
+
+int main(){
+  duom asmuo1, masyvas[25];
+  int n, m;
+  cout<<"Iveskite studentu kieki: "; cin>>n;
+  for (int i=0;i<n;i++) {masyvas[i]=ivedimas(m);}
+  cout<<std::left<<std::setw(15)<< "Vardas" <<std::left<<std::setw(15)<< "Pavarde" << std::left<<std::setw(20)<<
+  "Galutinis(vid.)" << std::left<<std::setw(20)<< "Galutinis(med.)" << endl;
+  for (int i=0;i<n; i++) {
+      isvedimas(masyvas[i], m);
+  }
+}
+
+duom ivedimas(int &n) {
+    int *dinMasyvas;
+    int s = 1;
+    int paz;
+    dinMasyvas = new int[s];
+    duom asmuo;
+    cout << "Iveskite varda: ";
+    cin >> asmuo.vardas;
+    cout << "Iveskite pavarde: ";
+    cin >> asmuo.pavarde;
+    cout << "Iveskite egzamino pazymi: ";
+    cin >> asmuo.egz;
+    string command = "N";
+    while (command != "T"){
+            cout << "Iveskite  pazymi: ";
+            cin >> dinMasyvas[s - 1];
+            s += 1;
+            cout << "Baigti pazymiu ivedima?(T/N): ";
+            cin >> command;
+            cout << endl;
+        }
+    for (int z = 0; z < (s - 1); z++) {paz += dinMasyvas[z];}
+    sort(dinMasyvas, dinMasyvas + s);
+    if ((s-1) % 2 == 0) {asmuo.med = double(dinMasyvas[(s-1)/2] + dinMasyvas[((s-1)/2)+1])/2;}
+    else asmuo.med = dinMasyvas[(s-1)/2];
+    asmuo.vid = (double)paz / (double)(s - 1);
+    return asmuo;
+    delete[] dinMasyvas;
+}
+
+void isvedimas(duom temp, int n){
+    cout<<std::left<<std::setw(15)<< temp.vardas <<std::left<<std::setw(15)<< temp.pavarde << std::left<<std::setw(20)<<
+        (temp.vid*0.4 + temp.egz*0.6) << std::left<<std::setw(20)<< (temp.med*0.4 + temp.egz*0.6) << endl;
+}
