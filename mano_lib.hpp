f4692763d156bd6aa5df34edcd1be336e4ae4771
norajas <113666841+norajas@users.noreply.github.com>
2022-09-16 16:24:59 +0300

    Update mano_lib.hpp

--- mano_lib.hpp
+++ mano_lib.hpp
@@ -4,11 +4,15 @@
 #include <vector>
 #include <iomanip>
 #include <algorithm>
+#include <cstdlib>
+#include <numeric>
 
 using std::string;
 using std::cout;
 using std::cin;
 using std::endl;
 using std::sort;
+using std::vector;
 using std::left;
 using std::setw;
+using std::setprecision;
