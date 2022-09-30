b81fcdfb30e46ca90c188105c76b372ee54c82ca
norajas <113666841+norajas@users.noreply.github.com>
2022-09-30 22:04:59 +0300

    Add files via upload

---
+++ funkcijos.hpp
@@ -0,0 +1,17 @@
+struct duom {
+    string vardas;
+    string pavarde;
+    double vid;
+    double med;
+    int egz;
+};
+
+duom ivedimas(int &n);
+
+duom generavimas(int &n);
+
+duom nuskaitymas();
+
+void isvedimasVid(duom temp, int n);
+
+void isvedimasMed(duom temp, int n);
