d4bae48b49e96d0701e6bf0f61005120e427d05a
norajas <113666841+norajas@users.noreply.github.com>
2022-10-13 17:46:29 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,8 +2,23 @@
 #include "funkcijos.hpp"
 
 int main() {
-    int n,m;
-    gen(1000);
-    vector<duom> stud = nusk(1000);
-    isv(stud);
+     gen(1000);
+    vector<duom> a = nusk(1000);
+    isv(a);
+    cout<<endl;
+    gen(10000);
+    vector<duom> b = nusk(10000);
+    isv(b);
+    cout<<endl;
+    gen(100000);
+    vector<duom> c = nusk(100000);
+    isv(c);
+    cout<<endl;
+    gen(1000000);
+    vector<duom> d = nusk(1000000);
+    isv(d);
+    cout<<endl;
+    gen(10000000);
+    vector<duom> e = nusk(10000000);
+    isv(e);
 };
