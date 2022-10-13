552b208a7c6f3dc6e63bc84dfa6f1debe9e82646
norajas <113666841+norajas@users.noreply.github.com>
2022-10-13 17:32:39 +0300

    Update funkcijos.cpp

--- funkcijos.cpp
+++ funkcijos.cpp
@@ -1,74 +1,46 @@
 #include "mano_lib.hpp"
 
-struct duom {
-    string vardas;
-    string pavarde;
-    double vid;
-    double med;
-    int egz;
+class Timer {
+private:
+    std::chrono::time_point<std::chrono::high_resolution_clock> start;
+public:
+    Timer() : start{std::chrono::high_resolution_clock::now()} {}
+
+    void reset() {
+        start = std::chrono::high_resolution_clock::now();
+    }
+
+    double elapsed() const {
+        return std::chrono::duration<double>
+                (std::chrono::high_resolution_clock::now() - start).count();
+    }
 };
 
-duom ivedimas(int &n) {
-    vector<int> paz;
-    int p;
-    int sum = 0;
-    duom asmuo;
-    cout << "Iveskite varda:";
-    cin >> asmuo.vardas;
-    cout << "Iveskite pavarde:";
-    cin >> asmuo.pavarde;
-    cout << "Iveskite egzamino pazymi:";
-    cin >> asmuo.egz;
-    string command = "N";
-    while (command != "T" && command != "t") {
-        cout << "Iveskite  pazymi:";
-        cin >> p;
-        paz.push_back(p);
-        cout << "Baigti pazymiu ivedima?(T/N)";
-        cin >> command;
-        cout << endl;
+string pazGeneravimas(){
+    vector<string> paz;
+    for(int i = 0; i < 5; i++){
+        paz.push_back(std::to_string(rand() % 10 + 1) + " ");
     }
-    sum = std::accumulate(paz.begin(), paz.end(), 0);
-    sort(paz.begin(), paz.end());
-    if ((paz.size() - 1) % 2 == 0) {
-        asmuo.med = double(paz[(paz.size() - 1) / 2] + paz[(paz.size() - 1) / 2 + 1]) / 2;
-    } else asmuo.med = paz[(paz.size() - 1) / 2];
-    asmuo.vid = (double) sum / (double) (paz.size());
-    return asmuo;
-    paz.clear();
-}
+    string p = std::accumulate(paz.begin(), paz.end(), std::string(""));
+    return p;
+};
 
-duom generavimas(int &n) {
-    vector<int> paz;
-    int sum, s;
-    duom asmuo;
-    cout << "Iveskite varda:";
-    cin >> asmuo.vardas;
-    cout << "Iveskite pavarde:";
-    cin >> asmuo.pavarde;
-    cout << "Iveskite egzamino pazymi:";
-    cin >> asmuo.egz;
-    cout << "Iveskite namu darbu pazymiu skaiciu:";
-    cin >> s;
-    for (int i = 0; i < s; i++) {
-        paz.push_back(rand() % 10 + 1);
-        cout << "Sugeneruotas pazymys:" << paz.back() << endl;
+void gen(int x){
+    Timer t;
+    string name = "stud"+std::to_string(x)+string(".txt");
+    ofstream failas(name);
+    for(int i = 1; i < x + 1; i++){
+        failas << "\tVardas"<<i<<"\tPavarde"<<i<< " " << pazGeneravimas() << endl;
     }
-    sum = accumulate(paz.begin(), paz.end(), 0);
-    sort(paz.begin(), paz.end());
-    if ((s - 1) % 2 == 0) { asmuo.med = double(paz[(s - 1) / 2] + paz[(s - 1) / 2 + 1]) / 2; }
-    else asmuo.med = paz[(s - 1) / 2];
-    asmuo.vid = (double) sum / (double) s;
-    return asmuo;
-    paz.clear();
-}
+    cout<< "Failo is " <<x<< " irasu sukurimo laikas:" << t.elapsed()<<endl;
+};
 
-duom nuskaitymas() {
+vector<duom> nusk(int x) {
+    Timer t;
+    string name = "stud"+std::to_string(x)+string(".txt");
+    std::ifstream failas(name);
     vector<duom> stud;
-    ifstream failas;
-    string pav;
-    failas.open("kursiokai.txt");
-    if (failas.is_open()) {
+    if (failas.is_open()){
         string eil;
         vector<int> paz;
         int p;
@@ -90,34 +62,43 @@ duom nuskaitymas() {
             }
             failas >> asmuo.egz;
             asmuo.vid = double(accumulate(paz.begin(), paz.end(), 0)) / 5;
-            asmuo.med = paz[4 / 2];
+            asmuo.gal = asmuo.vid * 0.4 + asmuo.egz * 0.6;
             stud.push_back(asmuo);
             paz.clear();
         }
-        sort(stud.begin(), stud.end(), [](const duom &a, const duom &b) {
-            return a.vardas < b.vardas;
-        });
-
-        cout << left << setw(15) << "Vardas" << left << setw(15) << "Pavarde" << left << setw(20) <<
-             "Galutinis(vid.)" << left << setw(20) << "Galutinis(med.)" << endl;
-        for (auto &i: stud) {
-            cout << left << setw(15) << i.vardas
-                 << left << setw(15) << i.pavarde
-                 << left << setw(20) << setprecision(3) << (i.vid * 0.4 + i.egz * 0.6)
-                 << left << setw(20) << setprecision(3) << (i.med * 0.4 + i.egz * 0.6) << endl;
-        }
         failas.close();
-    } else { cout << "Klaida atidarant faila" << endl; }
-}
+        cout<< "Failo is " <<x<< " irasu nuskaitymo laikas:" << t.elapsed() <<endl;
+    }
+    else cout << "Klaida atidarant faila";
+    return stud;
 
-void isvedimasVid(duom temp, int n) {
-    cout << left << setw(15) << temp.vardas
-         << left << setw(15) << temp.pavarde
-         << left << setw(20) << setprecision(3) << (temp.vid * 0.4 + temp.egz * 0.6) << endl;
-}
+};
 
-void isvedimasMed(duom temp, int n) {
-    cout << left << setw(15) << temp.vardas
-         << left << setw(15) << temp.pavarde
-         << left << setw(20) << setprecision(3) << (temp.med * 0.4 + temp.egz * 0.6) << endl;
+void isv(vector<duom> v){
+    duom asmuo;
+    Timer t;
+    vector<duom> m;
+    vector<duom> n;
+    for (int i = 0; i < v.size(); i++){
+        asmuo = v[i];
+        if (asmuo.gal >= 5.0){
+            m.push_back(asmuo);
+        }
+        else n.push_back(asmuo);
+    }
+    cout<< "Studentu isskirstymas i 2 grupes uztruko:" << t.elapsed()<<endl;
+    t.reset();
+    ofstream f1("moksliukai");
+    ofstream f2("nevykeliai");
+    for (int i = 0; i < m.size(); i++){
+        asmuo = m[i];
+        f1 << left << setw(15) << asmuo.vardas
+           << left << setw(15) << asmuo.pavarde << left << setw(15) << asmuo.gal << endl;
+    }
+    for (int i = 0; i < n.size(); i++){
+        asmuo = n[i];
+        f2 << left << setw(15) << asmuo.vardas
+              << left << setw(15) << asmuo.pavarde << left << setw(15) << asmuo.gal << endl;
+    }
+    cout<< "Studentu isskirstymas i 2 failus uztruko:" << t.elapsed()<<endl;
 }
