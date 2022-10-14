3fc13659efdf378ee306c0d283663a20b8433eda
norajas <113666841+norajas@users.noreply.github.com>
2022-10-14 08:44:14 +0300

    Update funkcijos.hpp

--- funkcijos.hpp
+++ funkcijos.hpp
@@ -1,12 +1,29 @@
+#include "mano_lib.hpp"
+
 struct duom {
     string vardas;
     string pavarde;
-    double vid;
     int egz;
     double gal;
+};
+
+class Timer {
+private:
+    std::chrono::time_point<std::chrono::high_resolution_clock> start;
+public:
+    Timer() : start{std::chrono::high_resolution_clock::now()} {}
+
+    void reset() {
+        start = std::chrono::high_resolution_clock::now();
+    }
+
+    double elapsed() const {
+        return std::chrono::duration<double>
+                (std::chrono::high_resolution_clock::now() - start).count();
+    }
 };
 
 void gen(int x);
 vector<duom> nusk(int x);
 string pazGeneravimas();
-void isv(vector<duom> v);
+void isv(vector<duom> v, string x);
