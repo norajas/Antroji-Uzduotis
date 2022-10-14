546c2369bcb8d522f5679954115e5b81f0293238
norajas <113666841+norajas@users.noreply.github.com>
2022-10-14 11:14:09 +0300

    Update funkcijos.hpp

--- funkcijos.hpp
+++ funkcijos.hpp
@@ -1,8 +1,9 @@
 #include "mano_lib.hpp"
-
 struct duom {
     string vardas;
     string pavarde;
+    double vid;
+    double med;
     int egz;
     double gal;
 };
@@ -23,7 +24,20 @@ public:
     }
 };
 
+duom ivedimas(int &n);
+
+duom generavimas(int &n);
+
+duom nuskaitymas();
+
+void isvedimasVid(duom temp, int n);
+
+void isvedimasMed(duom temp, int n);
+
 void gen(int x);
+
 vector<duom> nusk(int x);
+
 string pazGeneravimas();
+
 void isv(vector<duom> v, string x);
